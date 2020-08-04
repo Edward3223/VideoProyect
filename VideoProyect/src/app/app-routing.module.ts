7c0a957a6e5bfef36025523506e33f76dc860c0b
Edward3223 <pepenr12@gmail.com>
2020-08-04 09:41:01 -0400

    Set routes

--- VideoProyect/src/app/app-routing.module.ts
+++ VideoProyect/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './Component/login/login.component';
+import { RegisterComponent } from './Component/register/register.component';
+import { HomeComponent } from './Component/home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'home', component: HomeComponent},
+  {path: '**' ,redirectTo: 'home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
