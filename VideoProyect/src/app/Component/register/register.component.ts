1e27c091b71e6049b52c9bd1a24dd5a7ba5c5228
Edward3223 <pepenr12@gmail.com>
2020-08-07 20:25:47 -0400

    Model user to send from firebase

--- VideoProyect/src/app/Component/register/register.component.ts
+++ VideoProyect/src/app/Component/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/Models/User.model';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  usuario: User;
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.usuario = new User()
+    this.usuario.Email = 'e.cabrera99@hotmail.com'
   }
 
+  onSubmit(){
+    console.log(this.usuario)
+  }
 }
